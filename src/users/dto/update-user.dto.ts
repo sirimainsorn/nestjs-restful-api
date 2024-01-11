d6028086b405ae08d268d5aa169996158da6a6c3
sirima insorn <sirimainsorn@gmail.com>
2024-01-11 22:14:05 +0700

    update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+// import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @ApiProperty()
+  userId: number;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  passwords: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,7 +10,7 @@ var userData = readFileSync(userPath).toString();
 
 @Injectable()
 export class UsersService {
-  users: User[] = JSON.parse(userData);
+  users: User[] = JSON.parse(userData) || [];
 
   async create(createUserDto: CreateUserDto) {
     this.users.push({
@@ -25,7 +25,7 @@ export class UsersService {
 
       return this.users;
     } catch (error) {
-      console.log(`WRITE ERROR: ${error}`);
+      console.log(`CREATE ERROR: ${error}`);
     }
   }
 
@@ -42,8 +42,21 @@ export class UsersService {
     return this.users.find((user) => user.userId === id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      const userIndex = this.users.findIndex((user) => user.userId === id);
+
+      if (userIndex > -1) {
+        this.users[userIndex] = updateUserDto;
+        const frameworksData = JSON.stringify(this.users);
+
+        writeFileSync(userPath, frameworksData, 'utf-8');
+
+        return updateUserDto;
+      }
+    } catch (error) {
+      console.log(`UPDATE ERROR: ${error}`);
+    }
   }
 
   remove(id: number) {
